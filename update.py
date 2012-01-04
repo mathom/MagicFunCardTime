49711f94f7aebcf7e4b55f91d54e4a40e97c43c3
Ryan Brandenburg <compscidictator@gmail.com>
2012-01-03 21:41:13 -0600

    Still working on pull_card

--- update.py
+++ update.py
@@ -3,32 +3,6 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 import re
-import sqlite3
-import os.path
-import atexit
-
-SQL_DB = None
-
-def db_handle():
-    global SQL_DB
-    if not SQL_DB:
-        SQL_DB = sqlite3.connect('magic.db')
-        atexit.register(db_close)
-    return SQL_DB
-
-def db_close():
-    global SQL_DB
-    SQL_DB.close()
-    SQL_DB = None
-
-def db_init():
-    qry = open('magic.sql', 'r').read()
-    conn = db_handle()
-    c = conn.cursor()
-    c.executescript(qry)
-    conn.commit()
-    c.close()
-    conn.close()
 
 
 def scrape(url):
@@ -38,20 +12,19 @@ def scrape(url):
 
 def grab_sets(*names):
     '''returns urls for desired sets'''
-    domain = 'http://magiccards.info/'
-    url = domain + 'sitemap.html'
+    url = 'http://magiccards.info/sitemap.html'
     html = scrape(url)
     english = [x for x in html('h2') if x.text.startswith('English')][0]
     table = english.findNext('table')
 
     results = []
     for h3 in [x for x in table.findAll('h3') if x.text in names]:
-        results += [(x.text, domain + x['href']) for x in h3.findNext('ul').findAll('a')]
+        results += [x['href'] for x in h3.findNext('ul').findAll('a')]
 
-    return results
+    return [url + '/' + x for x in results]
 
 
-def pull_set(edition, url):
+def pull_set(url):
     '''returns the list of urls'''
     base_url = "http://magiccards.info"
     html = scrape(url)
@@ -61,8 +34,9 @@ def pull_set(edition, url):
     for tr in trs:
         link = tr.findAll('a')[0]
         url = base_url + str(link['href'])
-        print "PULLING CARD %s" % link.text
-        pull_card(url)
+        pull_card(x)
+#     urls = [base_url + str(link).split('">')[0][10:] for link in links]
+#     [pull_card(x) for x in urls]
     
 
 def pull_card(url):
@@ -78,16 +52,24 @@ def pull_card(url):
     result["illus"] = info_td_ps[3].text
 
     uls = info_td.findAll('ul')
-    rulings = uls[0]
-    legality = uls[1]
+    try:
+        rulings = uls[0]
+    except IndexError:
+        rulings = None
+    try:
+        legality = uls[1]
+    except IndexError:
+        rulings = None
     small = info_td.findNext('td').findChild('small')
     try:
-        result["other_parts"] = small.findNext('u')
+        result["other_parts"] = [x for x in small.findAll('u') if x.text.find('The other part is:') >= 0][0].findNext('a')
     except:
         result["other_parts"] = None
-    '''other_parts
-    printings
-    editions
+    #try:
+    print [x for x in small.findAll('u') if x.text.find('Printings:') >= 0][0].findNext('b')
+    #except:
+     #   result["printings"] = None
+    '''editions
     cast_cost
     abilities
     powers
@@ -97,12 +79,7 @@ def pull_card(url):
 
 
 if __name__=='__main__':
-
-    if not os.path.isfile("magic.db"):
-        db_init()
-
     updated_sets = grab_sets('Expansions', 'Core Sets')
-    [pull_set(*x) for x in updated_sets]
-
-    db_close()
+    print updated_sets
+    [pull_set(x) for x in updated_sets]
 
