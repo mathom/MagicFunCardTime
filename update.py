3d36037e709fe8235420229514945e8b7bf53393
Matthew Thompson <chameleonator@gmail.com>
2012-01-03 19:28:59 -0600

    adding more

--- update.py
+++ update.py
@@ -3,6 +3,13 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
+def grab_sets(names):
+    '''returns urls for desired sets'''
+    return []
+
 
 if __name__=='__main__':
-    pass
+    updated_sets = grab_sets('Expansions', 'Core Sets')
+
+    [pull_set(x) for x in updated_sets]
+
